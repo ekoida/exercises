2551e070f99fc2f591244ec0f9bbbd43d16cd48c
ekoida <celeno4ka@gmail.com>
2023-10-09 21:08:11 +0300

    init commit - exercise 1 - create list of scrorpions albums

---
+++ script.js
@@ -0,0 +1,9 @@
+import { showListOfSongs } from "./show_list_of_songs.js";
+
+////////////////////////////////////////////////
+// Show list of songs function implementation //
+///////////////////////////////////////////////
+const showSongsButton = document.querySelector(".show_songs");
+showSongsButton.addEventListener("click", showListOfSongs);
+
+/// Rest of the functions
---
+++ show_list_of_songs.js
@@ -0,0 +1,30 @@
+const listOfScorpionsAlbums = [
+  "Lonesome Crow",
+  "Fly to the Rainbow",
+  "In Trance",
+  "Virgin Killer",
+  "Taken by Force",
+  "Lovedrive",
+  "Animal Magnetism",
+  "Blackout",
+  "Love at First Sting",
+  "Savage Amusement",
+  "Crazy World",
+  "Face the Heat",
+  "Pure Instinct",
+  "Eye II Eye",
+  "Unbreakable",
+  "Humanity: Hour I",
+  "Return To Forever",
+  "Rock Believer",
+];
+
+export function showListOfSongs() {
+  // 1 - найти элемент ul, куда будем вписывать названия альбомов
+  // 2 - создать элемент списка li, например так const li = document.createElement('li')
+  // 3 - в цикле пройти по всем альбомам из массива
+  //    4 - клонивать созданный элемент списка li, например так const currentLi = li.clone()
+  //    5 - добавить в клонированый элемент списка название альбома
+  //    6 - добавить клонированый элемент в элемент ul,  который мы нашли в пункте №1
+  console.log("hello form show list of songs");
+}
