00a2786f20da773e01cb1b12fbdff6af8b5efab2
ekoida <celeno4ka@gmail.com>
2023-10-12 16:34:52 +0300

    created list of scrorpions albums dynamically

--- show_list_of_songs.js
+++ show_list_of_songs.js
@@ -21,10 +21,16 @@ const listOfScorpionsAlbums = [
 
 export function showListOfSongs() {
   // 1 - найти элемент ul, куда будем вписывать названия альбомов
+  const ul=document.querySelector(".list_of_songs");
   // 2 - создать элемент списка li, например так const li = document.createElement('li')
+  const li=document.createElement("li");
   // 3 - в цикле пройти по всем альбомам из массива
-  //    4 - клонивать созданный элемент списка li, например так const currentLi = li.clone()
-  //    5 - добавить в клонированый элемент списка название альбома
+  for (let i=0; i < listOfScorpionsAlbums.length; i++) {
+  //    4 - клонивать созданный элемент списка li, например так const currentLi = li.cloneNode(true)
+  const currentLi = li.cloneNode(true);
+    //    5 - добавить в клонированый элемент списка название альбома
+currentLi.textContent=listOfScorpionsAlbums[i]
   //    6 - добавить клонированый элемент в элемент ul,  который мы нашли в пункте №1
-  console.log("hello form show list of songs");
+    ul.appendChild(currentLi);
+  }
 }
