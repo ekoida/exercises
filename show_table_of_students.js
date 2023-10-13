942f85d90d90335343301548a8686fac2a942c0f
ekoida <celeno4ka@gmail.com>
2023-10-13 18:56:27 +0300

    rearranged a bit code

--- show_table_of_students.js
+++ show_table_of_students.js
@@ -36,30 +36,29 @@ const listOfStudents = [
 
 export function showTableOfStudents() {
   // 1 - найти элемент tbody по классу
-  const tbody=document.querySelector(".students_data");
+  const tbody = document.querySelector(".students_data");
   // 2 - создать элемент tr
-  const tr=document.createElement("tr");
+  const tr = document.createElement("tr");
   // 3 - создать элемент td
-  const td=document.createElement("td");
+  const td = document.createElement("td");
   // 4 - удалить tr c записью No Data
   tbody.removeChild(tbody.querySelector("tr"));
 
   // 5 - создать цикл для обхода массива с данными студентов
-  for (let i=0; i < listOfStudents.length; i++){
+  for (let i = 0; i < listOfStudents.length; i++) {
+    //   7 - клонировать tr
     const currentTr = tr.cloneNode(true);
+    //   8 - клонировать td  для каждого свойства студента (id, имя и оценка)
     const currentTdId = td.cloneNode(true);
     const currentTdName = td.cloneNode(true);
     const currentTdGrade = td.cloneNode(true);
-    currentTdId.textContent=listOfStudents[i].sdudentId;
-    currentTdName.textContent=listOfStudents[i].studentName;
-    currentTdGrade.textContent=listOfStudents[i].grade;
-    currentTr.append(currentTdId,currentTdName,currentTdGrade);
+    //   9 - записть в каждый клониваный td соответвующую информацию
+    currentTdId.textContent = listOfStudents[i].sdudentId;
+    currentTdName.textContent = listOfStudents[i].studentName;
+    currentTdGrade.textContent = listOfStudents[i].grade;
+    //   10 - добавить клонированные td в клонированный tr
+    currentTr.append(currentTdId, currentTdName, currentTdGrade);
+    //   11 - добавить клонированый tr в tbody
     tbody.appendChild(currentTr);
   }
-  // 6 - внутри цикла:
-  //   7 - клонировать tr
-  //   8 - клонировать td  для каждого свойства студента (id, имя и оценка)
-  //   9 - записть в каждый клониваный td соответвующую информацию
-  //   10 - добавить клонированные td в клонированный tr
-  //   11 - добавить клонированый tr в tbody
 }
